9213691e19aa03a7b82b3fe2767f7fe903641f22
Matheus Kenzo <100180936+Dobat00@users.noreply.github.com>
2024-04-19 16:54:14 -0300

    Categorias adicionadas

--- ftracker/main/models.py
+++ ftracker/main/models.py
@@ -4,9 +4,15 @@ from django.db import models
 
 
 class Gasto(models.Model):
+    categoria_lista = [
+        ('Contas do mes', 'Contas do mes'),
+        ('Lazer', 'Lazer'),
+        ('Compras', "Compras"),
+    ]
     nome = models.CharField(max_length=200)
     descricao = models.CharField(max_length=200)
     valor = models.FloatField()
+    categoria = models.CharField(max_length=200, choices=categoria_lista, null=True)
 
     def __str__(self):
         return self.nome
